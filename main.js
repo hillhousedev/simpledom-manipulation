f814146641e2747a8a19cff7b2bca7e3ea1da819
hillhousedev <adesemoyeolusola@gmail.com>
2019-01-31 11:19:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,112 @@
+
+const myHeading = document.getElementsByTagName('h1')[0];
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
+
+const errorNotBlue = document.getElementsByClassName('error-not-blue');
+
+const evens = document.querySelectorAll('li:nth-child(even)');
+
+const romanceItems = document.querySelectorAll('nav#myNav ul li');
+
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+
+const removeItemButton = document.querySelector('button.removeItemButton');
+
+const listItems = document.querySelectorAll('li');
+
+const firstUl = document.querySelector('ul');
+
+const attachButtons = (li) => {
+    let up = document.createElement('button');
+    up.className = 'up';
+    up.textContent = 'Up';
+    li.appendChild(up);
+
+    let down = document.createElement('button');
+    down.className = 'down';
+    down.textContent = 'Down';
+    li.appendChild(down);
+
+    let remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.textContent = 'Remove Item';
+    li.appendChild(remove);
+};
+
+firstUl.addEventListener('click', (e) => {
+    let li = e.target.parentNode;
+    let ul = li.parentNode;
+    if(event.target.tagName === 'BUTTON' && event.target.className === 'remove') {
+        ul.removeChild(li);
+    }
+
+    if(event.target.tagName === 'BUTTON' && event.target.className === 'up') {
+        if (li.previousElementSibling) {
+            ul.insertBefore(li, li.previousElementSibling);
+        }
+        
+    }
+
+    if(event.target.tagName === 'BUTTON' && event.target.className === 'down') {
+        if(li.nextElementSibling) {
+            ul.insertBefore(li.nextElementSibling, li);
+        }
+    }
+});
+
+myButton.addEventListener('click', () => {
+    myHeading.style.color = myTextInput.value;
+    for(let i = 0; i < myList.length; i++) {
+        myList[i].style.color = myTextInput.value;
+    }
+    for(let j = 0; j < errorNotBlue.length; j++) {
+        errorNotBlue[j].style.color = 'red';
+    }
+});
+
+for (let k = 0; k < evens.length; k++) {
+    evens[k].style.backgroundColor = 'lightgray';
+}
+
+descriptionButton.addEventListener('click', () => {
+    descriptionP.textContent = descriptionInput.value + ':';
+    descriptionInput.value = '';
+});
+
+toggleList.addEventListener('click', () => {
+    if (listDiv.style.display == 'none') {
+        toggleList.textContent = 'Hide List';
+        listDiv.style.display = 'block';
+    } else {
+        toggleList.textContent = 'Show list';
+        listDiv.style.display = 'none';
+    }
+});
+
+
+addItemButton.addEventListener('click', () => {
+    let newListItem = document.createElement('li');
+    newListItem.textContent = addItemInput.value;
+    attachButtons(newListItem);
+
+    let firstList = document.querySelector('ul.firstList');
+    firstList.appendChild(newListItem);
+    addItemInput.value = '';
+
+});
+
+removeItemButton.addEventListener('click', () => {
+    let firstList = document.querySelector('ul.firstList');
+    let lastChild = document.querySelector('ul.firstList li:last-child');
+    firstList.removeChild(lastChild);
+});
